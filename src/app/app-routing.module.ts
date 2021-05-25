257e53fcf9ea829023d4464fa7070d9bbfbcecc6
Anthony van Deventer <anthony.vandeventer@standardbank.co.za>
2021-05-25 17:10:59 +0200

    added routes + routing events to header

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const routes: Routes = [
+  { path: "recipe", component: RecipeComponent },
+  { path: "shopping-list", component: ShoppingListComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { HeaderComponent } from './navigation/header/header.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { DropdownDirective } from './shared/dropdown.directive';
+import { AppComponent } from "./app.component";
+import { RecipeListComponent } from "./recipe/recipe-list/recipe-list.component";
+import { RecipeItemComponent } from "./recipe/recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./recipe/recipe-detail/recipe-detail.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingListEditComponent } from "./shopping-list/shopping-list-edit/shopping-list-edit.component";
+import { HeaderComponent } from "./navigation/header/header.component";
+import { RecipeComponent } from "./recipe/recipe.component";
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -22,13 +23,10 @@ import { DropdownDirective } from './shared/dropdown.directive';
     ShoppingListEditComponent,
     HeaderComponent,
     RecipeComponent,
-    DropdownDirective
+    DropdownDirective,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
